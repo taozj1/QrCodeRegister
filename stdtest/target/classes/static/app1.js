cc5928bbbbd86f6bec9786643a030b3f0fc32b79
taozj1 <60253186+taozj1@users.noreply.github.com>
2021-12-06 22:58:41 +0800

    Add files via upload

---
+++ stdtest/target/classes/static/app1.js
@@ -0,0 +1,9 @@
+const wwLogin = new WwLogin({
+    "id": "wxLoginCode",
+    "appid": "ww5f3c78aa1ea4bbdc",
+    "agentid": "1000028",
+    "redirect_uri": "http://3965v681u4.zicp.vip/getCode",
+    "state": "tzj",
+    "href": "",
+    "lang": "zh",
+});
